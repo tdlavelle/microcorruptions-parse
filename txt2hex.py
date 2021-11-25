569904fedfd9a439087fc90e9b925f65d4272800
Thomas Lavelle <tdlavelle@gmail.com>
2021-11-24 20:31:20 -0500

    fixed being an idiot with handling gaps

--- txt2hex.py
+++ txt2hex.py
@@ -14,25 +14,16 @@ for line in raw_lines:
         continue
     # check for continuous address space, add 0s if not
     current_address = int(line[:4], base=16)
-    if(last_address == 0 and current_address == 0):
-        # raw file starts at 0x0000, process normal
-        last_address = current_address
-    elif(current_address > last_address+6):
-        # jump occurred in address space, fill with 0s
-        print("current address = "+hex(current_address)+" last address = "+hex(last_address)+" last written = "+hex(parse_file.tell()))
-        for i in range(current_address-parse_file.tell()):
+    next_address = parse_file.tell()
+    if (current_address > next_address):
+        # gap found, fill
+        for i in range(current_address-next_address):
             parse_file.write(bytes.fromhex("00"))
-        last_address = current_address
-    elif(current_address >= last_address+2):
-        # normal processing of contiguous memory, instructions can be 2 or 4 bytes
-        last_address = current_address
 
     # handle program strings
     if('"' in line[:20]):
         string_bytes = bytes(line[6:].replace('"','').replace("\n","\0"), 'utf-8')
         parse_file.write(string_bytes)
-        # jump last_address to account for string length being written
-        last_address = current_address + len(string_bytes)
         continue
     # extract only the bytes
     hex_strings = line[7:21].split()
