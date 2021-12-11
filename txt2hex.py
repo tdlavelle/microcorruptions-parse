6ac6c4baaa90a7eac6add85decc524e632167c3e
Thomas Lavelle <tdlavelle@gmail.com>
2021-12-11 11:20:30 -0500

    Squashed commit of the following:
    commit 87cf20480d8ab9c589fab217353c553a9ffc0887
    Author: Thomas Lavelle <tdlavelle@gmail.com>
    Date:   Sat Dec 11 11:16:40 2021 -0500
    
        removed file hardcoding plus other cleanup

--- txt2hex.py
+++ txt2hex.py
@@ -1,11 +1,22 @@
-raw_file = open('data/neworleans.txt', "r")
-parse_file = open('data/neworleans.bin', "wb")
-labels_file = open('data/neworleans.labels', "w")
+import sys
 
 TOTAL_MEMORY_SIZE = 0xFFFF
 
-raw_lines = raw_file.readlines()
+# expect txt file as first (and only) input
+if(len(sys.argv) != 2):
+    print("USAGE: "+sys.argv[0]+" <text file to parse>")
+    exit()
 
+# create file names and files from input
+raw_file_name = sys.argv[1]
+base_file_name = raw_file_name.rsplit(".", 1)[0]
+
+raw_file = open(raw_file_name, "r")
+bin_file = open(base_file_name+".bin", "wb")
+labels_file = open(base_file_name+".labels", "w")
+
+# begin parsing
+raw_lines = raw_file.readlines()
 for line in raw_lines:
     # get rid of extra new lines
     if(not line.strip()): 
@@ -16,32 +27,30 @@ for line in raw_lines:
         continue
     # check for continuous address space, add 0s if not
     current_address = int(line[:4], base=16)
-    next_address = parse_file.tell()
+    next_address = bin_file.tell()
     if (current_address > next_address):
         # gap found, fill
         for i in range(current_address-next_address):
-            parse_file.write(bytes.fromhex("00"))
+            bin_file.write(bytes.fromhex("00"))
+        # no continue because the line with the gap still needs to be captured below
 
     # handle program strings
     if('"' in line[:20]):
         string_bytes = bytes(line[6:].replace('"','').replace("\n","\0"), 'utf-8')
-        parse_file.write(string_bytes)
+        bin_file.write(string_bytes)
         continue
     # extract only the bytes
     hex_strings = line[7:21].split()
     for hex_string in hex_strings:
         hex_bytes = bytes.fromhex(hex_string)
-        parse_file.write(hex_bytes)
+        bin_file.write(hex_bytes)
 
-size = parse_file.tell()-1
-#print("size = "+hex(size)+" total = "+hex(TOTAL_MEMORY_SIZE))
 # check if padding needed at end
+size = bin_file.tell()-1
 if(size < TOTAL_MEMORY_SIZE):
     for i in range(TOTAL_MEMORY_SIZE-size):
-        parse_file.write(bytes.fromhex("00"))
-
-#print("end addr = "+hex(parse_file.tell()))
+        bin_file.write(bytes.fromhex("00"))
 
 labels_file.close()
 raw_file.close()
-parse_file.close()
+bin_file.close()
